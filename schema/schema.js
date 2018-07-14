05769425b3fb28c7c514536efe3c412fe7826af9
Gene Hynson <ghynson3@gatech.edu>
2018-07-14 16:55:13 -0400

    db setup using json server, root query has user and company types defined

--- schema/schema.js
+++ schema/schema.js
@@ -6,11 +6,18 @@ const {
     GraphQLInt,
     GraphQLSchema,
 } = graphql;
+const axios = require('axios');
 
-const users = [
-    { id: '23', firstName: 'Bill', age: 20 },
-    { id: '47', firstName: 'Samantha', age: 21 },
-];
+const db = 'http://localhost:3000';
+
+const CompanyType = new GraphQLObjectType({
+    name: 'Company',
+    fields: {
+        id: { type: GraphQLString },
+        name: { type: GraphQLString },
+        description: { type: GraphQLString }
+    }
+});
 
 const UserType = new GraphQLObjectType({
     name: 'User',
@@ -18,6 +25,13 @@ const UserType = new GraphQLObjectType({
         id: { type: GraphQLString },
         firstName: { type: GraphQLString },
         age: { type: GraphQLInt },
+        company: { 
+            type: CompanyType,
+            resolve(parentValue, args) {
+                return axios.get(`${db}/companies/${parentValue.companyId}`)
+                    .then(resp => resp.data);
+            }            
+        }
     },
 });
 
@@ -28,7 +42,16 @@ const RootQuery = new GraphQLObjectType({
             type: UserType,
             args: { id: { type: GraphQLString } },
             resolve(parentValue, args) {
-                return _.find(users, { id: args.id });
+                return axios.get(`${db}/users/${args.id}`)
+                    .then(resp => resp.data);
+            }
+        },
+        company: {
+            type: CompanyType,
+            args: { id: { type: GraphQLString } },
+            resolve(parentValue, args) {
+                return axios.get(`${db}/companies/${args.id}`)
+                    .then(resp => resp.data);
             }
         }
     },
